badab31dbb24beeaef50b2af9aa6d7ad948c2c52
sabyasachi008 <163sabyasachighoshxiis@gmail.com>
2025-12-27 12:59:27 +0530

    feat(client): create react component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
+import sqlLogo from './assets/sql-logo.png'
+import styles from './index.module.css'
 import { useState } from 'react'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [queryDescription, setQueryDescription] = useState("");
+  
+  const onSubmit= (e) => {
+    e.preventDefault();
+    console.log("form Submitted :",queryDescription)
+  };
   return (
-    <h1>Hello World</h1>
+      <main className={styles.main}>
+      <img src={sqlLogo} alt="" className={styles.icon}/>
+      <h2>Generate SQL with AI</h2>
+      <form onSubmit={onSubmit}>
+        <input 
+        type="text"
+        name="query-description"
+        placeholder="Describe your Query" 
+        onChange={(e) => setQueryDescription(e.target.value)}     //on change it would take the e(Event).target.value
+        />
+        <input type="submit" 
+        value="Generate Query"
+        />
+      </form>
+    </main>
   )
 }
 
 export default App
+ 
